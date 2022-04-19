c0eca2587883e03dc174470571afff054db351fa
KyungIn Lee <jess.lee2906@gmail.com>
2022-04-20 09:19:17 +1000

    initial commit and file setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+      { type: 'list', message: ""}
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
