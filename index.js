87b7557f055be651a7398582686d65e40fb45e54
KyungIn Lee <jess.lee2906@gmail.com>
2022-04-20 11:47:59 +1000

    Functions to present question array and store user input, generate readme writeup in output folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const generateReadme = require('./config/generateReadme');
 const axios = require('axios');
-const { fstat } = require('fs');
+const fs = require('fs')
 
 // prompts requiring user input
 const questions = [
@@ -79,27 +79,36 @@ const questions = [
     { 
         type: 'list', 
         message: "Choose your license",
-        choices: ['agpl', 'apache', 'mit', 'no license'],
+        choices: ['mit', 'agpl', 'apache', 'no license'],
         name: "license"
     }
 ];
 
-// Generate README function
-const generateFile = fileContent => {
+// Generate README function using fs writeFile with promise error handling
+const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./output/README.md', fileContent, err => {
             if (err) {
-                reject(err);
+                reject(`Error during setup: ${err}`);
                 return;
             }
 
             resolve({
-                ok: true,
+                status: true,
                 message: "README complete!"
             });
         });
     });
 };
+
+//Present question array and store user input
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(userInput => {
+        return userInput;
+    })
+}
+
 
 inquirer
   .prompt([
