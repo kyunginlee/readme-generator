7b0d35c8ee773b11edb70c151d23835946bad64f
KyungIn Lee <jess.lee2906@gmail.com>
2022-04-20 10:17:44 +1000

    relevant files to gitignore

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
+// Packages required for application
 const inquirer = require('inquirer');
+const 
+const axios = require('axios');
+const questions = [
+    { 
+        type: 'input', 
+        message: "Your project title",
+        name: "title"
+    },
+    { 
+        type: 'input', 
+        message: "Short description explaining your project",
+        name: "description"
+    },
+    { 
+        type: 'input', 
+        message: "Table of Contents",
+        name: "contents"
+    },
+    { 
+        type: 'input', 
+        message: "Steps to install your project",
+        name: "installation"
+    },
+    { 
+        type: 'input', 
+        message: "Provide instructions and examples for use",
+        name: "usage"
+    },
+    { 
+        type: 'input', 
+        message: "List contributors and collaborators along with their github profile and any resources",
+        name: "usage"
+    },
+    { 
+        type: 'input', 
+        message: "Choose your license",
+        name: "license"
+    }
+];
+
 inquirer
   .prompt([
       { type: 'list', message: ""}
